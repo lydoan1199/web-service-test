e162c2b2bc9646aed7eb3bdd9933132f48a7d704
ly.doan <lydoan@kms-technology.com>
2022-01-14 17:12:09 +0700

    Add TC

---
+++ Test Suites/TestSuite contain TC deleted from KS.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite contain TC deleted from KS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ee83fafe-3046-4128-9da8-085b58d1fe50</testSuiteGuid>
+   <testCaseLink>
+      <guid>2acd1b93-13f0-4f85-848b-234c48fef4e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Deleted TC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
