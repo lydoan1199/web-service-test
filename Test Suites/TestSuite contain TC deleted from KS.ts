dd68a55dabe6d68462fb038fdb5086f44090fed4
ly.doan <lydoan@kms-technology.com>
2022-01-14 17:17:04 +0700

    remove TC

--- Test Suites/TestSuite contain TC deleted from KS.ts
+++ Test Suites/TestSuite contain TC deleted from KS.ts
@@ -11,10 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>ee83fafe-3046-4128-9da8-085b58d1fe50</testSuiteGuid>
-   <testCaseLink>
-      <guid>2acd1b93-13f0-4f85-848b-234c48fef4e3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Deleted TC</testCaseId>
-   </testCaseLink>
 </TestSuiteEntity>
