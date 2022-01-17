df15ceeeed1dfacaff48e187827e208eed5fb54a
ly.doan <lydoan@kms-technology.com>
2022-01-17 16:58:26 +0700

    add tc

---
+++ Test Suites/Deleted/New Test Suite).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7c3a36f0-0708-42e8-a8b6-3b52b80935da</testSuiteGuid>
+</TestSuiteEntity>
