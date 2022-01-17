707df4263f129fb82ba557753768055cb6cc3c1e
ly.doan <lydoan@kms-technology.com>
2022-01-17 17:02:11 +0700

    add tc

--- Test Suites/TS-1 from ks.ts
+++ Test Suites/TS-1 from ks.ts
@@ -11,10 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>dee3f5cb-e9e2-43a4-bad0-6a59b3331172</testSuiteGuid>
-   <testCaseLink>
-      <guid>c32f9282-091f-4684-b97e-1d3e497cf5fc</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Deleted 2</testCaseId>
-   </testCaseLink>
 </TestSuiteEntity>
