157bb7779f2401b7c26519a85652e22fc85fe56c
ly.doan <lydoan@kms-technology.com>
2022-01-17 13:40:50 +0700

    add tc

---
+++ Test Suites/Deleted/TS from KS.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS from KS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ded1c63a-a56d-4355-83c4-ff9ce58026ba</testSuiteGuid>
+   <testCaseLink>
+      <guid>b7ca6720-ebd4-4fa4-9abe-40f07fb7ac4e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Deleted tc 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>abf01f14-4f96-490e-a734-5548c87c93fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Nothing</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
