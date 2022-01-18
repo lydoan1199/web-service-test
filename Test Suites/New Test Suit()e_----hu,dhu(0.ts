988fea6ff0208ac063003d4bcb5cc04a7b19d25d
ly.doan <lydoan@kms-technology.com>
2022-01-18 10:49:42 +0700

    add profile

--- Test Suites/New Test Suit()e_----hu,dhu(0.ts
+++ Test Suites/New Test Suit()e_----hu,dhu(0.ts
@@ -5,10 +5,82 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>12f196db-1271-49c4-9c26-aea866852766</testSuiteGuid>
+   <testCaseLink>
+      <guid>d899bf08-03fe-4993-907a-6f91dd2e0b4a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Find user by ID</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c01f9799-0b50-4449-9381-55ef5499f42a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c286e457-b6f1-4251-b3ff-4b8732ca5dd9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>dc24082d-c647-404f-aad4-147039c3b351</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c61e367d-e8d2-440b-8b62-b1afb19bf7e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>12db7b5a-6bfc-4927-83be-322afbf82b91</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96522480-c793-4b15-8403-dc51388ca1ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create a new user</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0de4fa76-1864-4d5e-aa8a-70ad8f4f4342</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>086a27a9-05ed-4e07-8465-e17bddfdeac1</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>311c2780-afd0-4352-a23f-fc00c2c42271</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bcddbada-1d5e-422c-b810-55f7c48bb931</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3b3cb3f6-19ff-4f44-ae2c-a1c680bd3044</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
