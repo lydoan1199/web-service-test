24f3be6c3648adc1aeac207130fa854bbfedd999
ly.doan <lydoan@kms-technology.com>
2022-01-18 09:29:52 +0700

    add profile

---
+++ Test Suites/New Test Suit()e_----hu,dhu(0.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suit()e_----hu,dhu(0</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>12f196db-1271-49c4-9c26-aea866852766</testSuiteGuid>
+</TestSuiteEntity>
