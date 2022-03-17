732b1b5fdd0bcee25a3c9e3b7ca08a28364e3523
ly.doan <lydoan@kms-technology.com>
2022-03-17 14:02:19 +0700

    ju

--- Test Suites/aaaaaa.ts
+++ Test Suites/aaaaaa.ts
@@ -15,6 +15,6 @@
       <guid>a3a1ce5f-682d-4fec-b166-c05161567d57</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Test Case 3</testCaseId>
+      <testCaseId>Test Cases/TC 1/TC 1.1/TC 1.1.1/Tc 1.1.1.1/TC 1.1.1.1.11/Test Case 3</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
